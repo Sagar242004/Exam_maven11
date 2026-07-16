63357a4d4d754583d3ba107ca2c026be363cda51
Sagar242004 <dksagar7624910281@gmail.com>
2026-07-16 19:59:33 +0530

    Update Main.java

--- src/main/java/com/skillsync/Main.java
+++ src/main/java/com/skillsync/Main.java
@@ -1,15 +1,13 @@
 package com.skillsync;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args)  {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("hello");
+    public static void main(String[] args) throws Exception {
 
-        for (int i = 1; i <= 3; i++) {//TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            System.out.println(i);
+        System.out.println("Application Started");
+
+        while (true) {
+            System.out.println("Application is running...");
+            Thread.sleep(5000);
         }
     }
 }
